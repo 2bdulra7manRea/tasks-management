25b42907addd01c1c8efe9d3aa9e62fe1521cf63
abdelrahmanMosa <abdomosa987@gmail.com>
2023-10-21 23:43:37 +0300

    added add function in history service

--- server/src/history/history.service.ts
+++ server/src/history/history.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { History } from './entities/history.entity';
 
 @Injectable()
-export class HistoryService {}
+export class HistoryService {
+  constructor(
+    @InjectRepository(History)
+    private historyRepository: Repository<History>,
+  ) {}
+
+  add(createHistory: any) {
+    return this.historyRepository.insert(createHistory);
+  }
+}
