79c63b6559f6c7821fcc970087ec353073573380
abdelrahmanMosa <abdomosa987@gmail.com>
2023-10-21 23:40:13 +0300

    added custom response interceptor

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerBuild } from './config/swagger';
+import { TransformInterceptor } from './common/interceptor/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   SwaggerBuild(app);
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.NODE_SERVER_PORT);
 }
 bootstrap();
