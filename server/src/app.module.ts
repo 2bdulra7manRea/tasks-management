f1bfe4a2a8111d329018e396e96145225e009e1f
abdelrahmanMosa <abdomosa987@gmail.com>
2023-10-21 22:10:53 +0300

    async database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,14 @@ import { TaskModule } from './task/task.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
 import { HistoryModule } from './history/history.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(databaseConfig),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => databaseConfig,
+    }),
     UserModule,
     TaskModule,
     HistoryModule,
